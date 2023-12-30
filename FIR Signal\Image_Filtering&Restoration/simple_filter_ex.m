4026f281a15e33de60584408094cfcb77d6f1891
RSqwining <147663623+RSqwining@users.noreply.github.com>
2023-12-30 13:54:25 -0800

    Update simple_filter_ex.m

--- FIR Signal\Image_Filtering&Restoration/simple_filter_ex.m
+++ FIR Signal\Image_Filtering&Restoration/simple_filter_ex.m
@@ -1,13 +1,33 @@
-xx = 256 * (rem(0:100, 50) < 10); %input coeff = [1, -0.9]; %filter coefficients
+xx = 256 * (rem(0:100, 50) < 10); %input 
+coeff = [1, -0.9]; %filter coefficients
 w = firfilt(coeff, xx); %output
 n = 0:75;
-figure(1);
-subplot(2, 1, 1); 
-stem(n, xx(n+1)); 
-xlabel('n'); ylabel('x[n]'); 
+l=0:1:22;
+r=0.9;
+b=r.^l;
+y=firfilt(b,w); %restoration output 
+subplot(4,1,1);
+stem(n,xx(n+1)); 
+xlabel('n'); 
+ylabel('x[n]'); 
 title('Input Signal'); 
-subplot(2, 1, 2); 
-stem(n, w(n+1)); 
+xlim([0,75]); 
+subplot(4,1,2); 
+stem(n,w(n+1));
 xlabel('n'); 
 ylabel('w[n]'); 
-title('Filtered Signal');
+title('Filtered Signal'); 
+xlim([0,75]);
+subplot(4,1,3);
+stem(n,y(n+1));
+xlabel('n'); 
+ylabel('y[n]'); 
+title('Restoration Filter Signal'); 
+xlim([0,75]);
+subplot(4,1,4); 
+stem(n,y(n+1)-xx(n+1)); 
+xlabel('n'); 
+ylabel('x[n]-y[n]'); 
+title('Difference (Error) Signal'); 
+xlim([0,50]);
+ylim([-30 200]);
